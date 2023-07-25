4f2c5fd8b5dafaba64bde0efb5816c3ff1634bfe
sanjayautomation <treestechnospace@gmail.com>
2023-07-25 21:47:48 +0530

    Second time commit

---
+++ src/main/java/LoginPage/Demo2.java
@@ -0,0 +1,11 @@
+package LoginPage;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Asish Demo2 class");
+		
+
+	}
+
+}
---
+++ src/test/java/LoginPageTest/Demo2.java
@@ -0,0 +1,11 @@
+package LoginPageTest;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Ashish Demo2 Class for login test");
+		
+
+	}
+
+}
